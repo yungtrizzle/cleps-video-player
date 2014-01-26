653481b3d67ce389069343378fca71856fdcc4e3
yungtrizzle <eshtonrob@gmail.com>
2014-01-26 17:27:51 +0800

    Functional interface along with play/pause

--- cleps_vidplayer.cpp
+++ cleps_vidplayer.cpp
@@ -1,9 +1,152 @@
 #include "cleps_vidplayer.h"
+#include <QVideoWidget>
+#include <QVideoSurfaceFormat>
 
 Cleps_VidPlayer::Cleps_VidPlayer(QWidget *parent)
-    : QMainWindow(parent)
+    : QWidget(parent)
+
+
 {
+    pState=2;
+    player = new QMediaPlayer;//(0, QMediaPlayer::VideoSurface);
+    playlist = new QMediaPlaylist;
+    player->setPlaylist(playlist);
+
+    videoWidget = new QVideoWidget;
+    fileMenu = new QMenu(tr("&File"));
+    QAction *opVid = new QAction(tr("&Open Video"), this);
+    QAction *quit = new QAction(tr("&Quit"), this);
+
+    opVid->setShortcut(QKeySequence::Open);
+    quit->setShortcut(QKeySequence::Quit);
+
+     connect(opVid, SIGNAL(triggered()), this, SLOT(open()));
+     connect(quit, SIGNAL(triggered()), this, SLOT(quit()));
+
+    fileMenu->addAction(opVid);
+    fileMenu->addAction(quit);
+
+    gblMenu = new QMenuBar();
+    gblMenu->addMenu(fileMenu);
+
+       playButton = new QPushButton;
+       playButton->setEnabled(false);
+       playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+
+       pause = new QPushButton;
+       pause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+
+
+      connect(playButton, SIGNAL(clicked()),this, SLOT(play()));
+
+
+    seekr = new QSlider(Qt::Horizontal);
+    seekr->setRange(0,0);
+
+    connect(seekr, SIGNAL(sliderMoved(int)),this, SLOT(setPosition(int)));
+
+
+       errorLabel = new QLabel;
+       errorLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
+
+
+    QBoxLayout *controlLayout = new QHBoxLayout;
+    controlLayout->setMargin(0);
+    controlLayout->addWidget(playButton);
+    controlLayout->addWidget(seekr);
+
+    QBoxLayout *layout = new QVBoxLayout;
+       layout->addWidget(gblMenu);
+       layout->addWidget(videoWidget);
+       layout->addLayout(controlLayout);
+       layout->addWidget(errorLabel);
+
+       setLayout(layout);
+       player->setVideoOutput(videoWidget);
+
+       connect(player, SIGNAL(stateChanged(QMediaPlayer::State)),this, SLOT(mediaStateChanged(QMediaPlayer::State)));
+       connect(player, SIGNAL(positionChanged(qint64)), this, SLOT(positionChanged(qint64)));
+       connect(player, SIGNAL(durationChanged(qint64)), this, SLOT(durationChanged(qint64)));
+      // connect(&player, SIGNAL(error(QMediaPlayer::Error)), this, SLOT(handleError()));
+
 }
+
+
+/*****************Public Slots*************/
+
+void Cleps_VidPlayer::open(){
+
+
+QString fileName = QFileDialog::getOpenFileName(this, tr("Open Video"), QDir::homePath());
+
+  if (!fileName.isEmpty()){
+
+      //player->setMedia(QUrl::fromLocalFile((fileName)));
+      playlist->addMedia(QUrl::fromLocalFile((fileName)));
+
+  }
+
+  playButton->setEnabled(true);
+
+}
+
+void Cleps_VidPlayer::play(){
+
+
+    switch(player->state()) {
+        case QMediaPlayer::PlayingState:
+            player->pause();
+            break;
+        default:
+            player->play();
+            break;
+
+}
+}
+
+
+void Cleps_VidPlayer::quit(){
+
+    this->close();
+
+}
+
+
+/*****************Private Slots*************/
+void Cleps_VidPlayer::mediaStateChanged(QMediaPlayer::State state)
+{
+    switch(state) {
+    case QMediaPlayer::PlayingState:
+        playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+        break;
+    default:
+        playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+        break;
+    }
+}
+
+void Cleps_VidPlayer::setPosition(int position){
+
+
+    player->setPosition(position);
+
+}
+
+void Cleps_VidPlayer::positionChanged(qint64 position){
+
+    seekr->setValue(position);
+}
+
+
+void Cleps_VidPlayer::durationChanged(qint64 timed){
+
+    seekr->setRange(0,timed);
+}
+
+
+
+
+
 
 Cleps_VidPlayer::~Cleps_VidPlayer()
 {
--- cleps_vidplayer.h
+++ cleps_vidplayer.h
@@ -1,15 +1,42 @@
 #ifndef CLEPS_VIDPLAYER_H
 #define CLEPS_VIDPLAYER_H
 
+#include <QtWidgets>
 #include <QMainWindow>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
 
-class Cleps_VidPlayer : public QMainWindow
+
+class Cleps_VidPlayer : public QWidget
 {
     Q_OBJECT
 
 public:
     Cleps_VidPlayer(QWidget *parent = 0);
     ~Cleps_VidPlayer();
+
+public slots:
+    void open();
+    void quit();
+    void play();
+
+private slots:
+    void mediaStateChanged(QMediaPlayer::State);
+    void setPosition(int);
+    void positionChanged(qint64);
+    void durationChanged(qint64);
+
+
+private:
+    QMediaPlayer *player;
+    QMediaPlaylist *playlist;
+    QSlider *seekr;
+    QMenuBar *gblMenu;
+    QMenu *fileMenu;
+    QAbstractButton *playButton, *pause;
+    QVideoWidget *videoWidget;
+    QLabel *errorLabel;
+    int pState;
 };
 
 #endif // CLEPS_VIDPLAYER_H
